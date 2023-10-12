3834feb3a9e0400f3fedfcfda5367c92ed0976aa
duawakainat <kainathafeez04@gmail.com>
2023-10-12 00:10:22 -0700

    first commit

---
+++ asset/bootstrap/js/app.js
@@ -0,0 +1,11 @@
+
+console.log('juj');
+$(window).load(function () {
+    console.log('i am ');
+    $('.progress-bar').css("width", "45%");
+});
+// $('.progress-bar').click(function (e) {
+//     $('body').css("background", "red")
+    
+
+// });
