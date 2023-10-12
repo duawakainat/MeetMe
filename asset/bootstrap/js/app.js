c3780d1a2034960de0190f673b3acdc86981f4c7
duawakainat <kainathafeez04@gmail.com>
2023-10-12 02:45:56 -0700

    change half

--- asset/bootstrap/js/app.js
+++ asset/bootstrap/js/app.js
@@ -1,11 +1,21 @@
 
 console.log('juj');
-$(window).load(function () {
-    console.log('i am ');
-    $('.progress-bar').css("width", "45%");
-});
+
 // $('.progress-bar').click(function (e) {
 //     $('body').css("background", "red")
-    
+
 
 // });
+$('.Db_btn_1').click(function (e) {
+    $('.Db_btn_2').css('background-color', 'white')
+    $('.Db_btn_2').css('color', 'black')
+    $('.Db_btn_1').css('background-color', '#ffffff1A')
+    $('.Db_btn_1').css('color', 'white')
+});
+$('.Db_btn_2').click(function (e) {
+    $('.Db_btn_1').css('background-color', '#ffffff1A')
+    $('.Db_btn_1').css('color', 'white')
+    $('.Db_btn_2').css('background-color', '#fff')
+    $('.Db_btn_2').css('color', 'black')
+});
+console.log('kainat');
